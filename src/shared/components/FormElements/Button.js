a3602e3c576f76a0dff11b458f95162eaa441f56
Emmanuel Oloke <dapopeemmanuel@gmail.com>
2022-05-19 02:42:26 +0100

    added modal to view map using state and react portal hooks

--- src/shared/components/FormElements/Button.js
+++ src/shared/components/FormElements/Button.js
@@ -3,15 +3,16 @@ import { Link } from 'react-router-dom';
 
 import './Button.css';
 
-const Button = props => {
+const Button = (props) => {
   if (props.href) {
     return (
       <a
-        className={`button button--${props.size || 'default'} ${props.inverse &&
-          'button--inverse'} ${props.danger && 'button--danger'}`}
+        className={`button button--${props.size || 'default'} ${
+          props.inverse && 'button--inverse'
+        } ${props.danger && 'button--danger'}`}
         href={props.href}
       >
-        {props.children}
+        {props.children}{' '}
       </a>
     );
   }
@@ -20,22 +21,24 @@ const Button = props => {
       <Link
         to={props.to}
         exact={props.exact}
-        className={`button button--${props.size || 'default'} ${props.inverse &&
-          'button--inverse'} ${props.danger && 'button--danger'}`}
+        className={`button button--${props.size || 'default'} ${
+          props.inverse && 'button--inverse'
+        } ${props.danger && 'button--danger'}`}
       >
-        {props.children}
+        {props.children}{' '}
       </Link>
     );
   }
   return (
     <button
-      className={`button button--${props.size || 'default'} ${props.inverse &&
-        'button--inverse'} ${props.danger && 'button--danger'}`}
+      className={`button button--${props.size || 'default'} ${
+        props.inverse && 'button--inverse'
+      } ${props.danger && 'button--danger'}`}
       type={props.type}
       onClick={props.onClick}
       disabled={props.disabled}
     >
-      {props.children}
+      {props.children}{' '}
     </button>
   );
 };
