3689890bb7e4bd3a6ca992037a7e4034be5f90dc
Emmanuel Oloke <dapopeemmanuel@gmail.com>
2022-05-26 18:30:00 +0100

    edited manifest.json to remove annoying console error and fixed minor issues on updateplace to show appropriate error when no place is available

--- src/places/pages/UpdatePlace.js
+++ src/places/pages/UpdatePlace.js
@@ -3,6 +3,7 @@ import { useParams } from 'react-router-dom';
 
 import Input from '../../shared/components/FormElements/Input';
 import Button from '../../shared/components/FormElements/Button';
+import Card from '../../shared/components/UIElements/Card';
 import {
   VALIDATOR_REQUIRE,
   VALIDATOR_MINLENGTH,
@@ -27,7 +28,7 @@ const DUMMY_PLACES = [
   },
   {
     id: 'p2',
-    title: 'Empire State Building',
+    title: 'Emp. State Building',
     description: 'One of the most famous skyscrappers in the world',
     imageUrl:
       'https://upload.wikimedia.org/wikipedia/commons/thumb/0/05/View_of_Empire_State_Building_from_Rockefeller_Center_New_York_City_dllu.jpg/640px-View_of_Empire_State_Building_from_Rockefeller_Center_New_York_City_dllu.jpg',
@@ -61,19 +62,21 @@ const UpdatePlace = (props) => {
   const identifiedPlace = DUMMY_PLACES.find((p) => p.id === placeId);
 
   useEffect(() => {
-    setFormData(
-      {
-        title: {
-          value: identifiedPlace.title,
-          isValid: true,
+    if (identifiedPlace) {
+      setFormData(
+        {
+          title: {
+            value: identifiedPlace.title,
+            isValid: true,
+          },
+          description: {
+            value: identifiedPlace.description,
+            isValid: true,
+          },
         },
-        description: {
-          value: identifiedPlace.description,
-          isValid: true,
-        },
-      },
-      true
-    );
+        true
+      );
+    }
     setIsLoading(false);
   }, [setFormData, identifiedPlace]);
 
@@ -85,7 +88,9 @@ const UpdatePlace = (props) => {
   if (!identifiedPlace) {
     return (
       <div className="center">
-        <h2>Could not find place!</h2>
+        <Card>
+          <h2>Could not find place!</h2>
+        </Card>
       </div>
     );
   }
--- src/places/pages/UserPlaces.js
+++ src/places/pages/UserPlaces.js
@@ -3,38 +3,36 @@ import { useParams } from 'react-router-dom'; // A React hook that gives us acce
 
 import PlaceList from '../components/PlaceList';
 
-const DUMMY_PLACES = [
-  {
-    id: 'p1',
-    title: 'Empire State Building',
-    description: 'One of the most famous skyscrappers in the world',
-    imageUrl:
-      'https://upload.wikimedia.org/wikipedia/commons/thumb/0/05/View_of_Empire_State_Building_from_Rockefeller_Center_New_York_City_dllu.jpg/640px-View_of_Empire_State_Building_from_Rockefeller_Center_New_York_City_dllu.jpg',
-    address: '20 W 34th St, New York, NY 10001',
-    location: {
-      lat: 40.7484445,
-      lng: -73.9878531,
+const DUMMY_PLACES = [{
+        id: 'p1',
+        title: 'Empire State Building',
+        description: 'One of the most famous skyscrappers in the world',
+        imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/0/05/View_of_Empire_State_Building_from_Rockefeller_Center_New_York_City_dllu.jpg/640px-View_of_Empire_State_Building_from_Rockefeller_Center_New_York_City_dllu.jpg',
+        address: '20 W 34th St, New York, NY 10001',
+        location: {
+            lat: 40.7484445,
+            lng: -73.9878531,
+        },
+        creator: 'u1',
     },
-    creator: 'u1',
-  },
-  {
-    id: 'p2',
-    title: 'Empire State Building',
-    description: 'One of the most famous skyscrappers in the world',
-    imageUrl:
-      'https://upload.wikimedia.org/wikipedia/commons/thumb/0/05/View_of_Empire_State_Building_from_Rockefeller_Center_New_York_City_dllu.jpg/640px-View_of_Empire_State_Building_from_Rockefeller_Center_New_York_City_dllu.jpg',
-    address: '20 W 34th St, New York, NY 10001',
-    location: {
-      lat: 40.7484445,
-      lng: -73.9878531,
+    {
+        id: 'p2',
+        title: 'Emp. State Building',
+        description: 'One of the most famous skyscrappers in the world',
+        imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/0/05/View_of_Empire_State_Building_from_Rockefeller_Center_New_York_City_dllu.jpg/640px-View_of_Empire_State_Building_from_Rockefeller_Center_New_York_City_dllu.jpg',
+        address: '20 W 34th St, New York, NY 10001',
+        location: {
+            lat: 40.7484445,
+            lng: -73.9878531,
+        },
+        creator: 'u2',
     },
-    creator: 'u2',
-  },
 ];
 const UserPlaces = () => {
-  const userId = useParams().userId; // This gives us access to the userId that is encoded in the Route Parameter
-  const loadedPlaces = DUMMY_PLACES.filter((place) => place.creator === userId); // Only returns an array of elements where creator matched the userId Route Parameter
-  return <PlaceList items={loadedPlaces} />;
+    const userId = useParams().userId; // This gives us access to the userId that is encoded in the Route Parameter
+    const loadedPlaces = DUMMY_PLACES.filter((place) => place.creator === userId); // Only returns an array of elements where creator matched the userId Route Parameter
+    return <PlaceList items = { loadedPlaces }
+    />;
 };
 
 export default UserPlaces;
