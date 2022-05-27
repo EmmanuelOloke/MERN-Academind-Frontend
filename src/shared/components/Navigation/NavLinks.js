248e7d136e121c3e8b5e353df13398d29d6191e3
Emmanuel Oloke <dapopeemmanuel@gmail.com>
2022-05-27 16:34:18 +0100

    adding auth context for app-wide state management

--- src/shared/components/Navigation/NavLinks.js
+++ src/shared/components/Navigation/NavLinks.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom'; // Special linking component that can analize URL and color current page link differently
-
 import './NavLinks.css';
 
 const NavLinks = (props) => {
@@ -12,13 +11,13 @@ const NavLinks = (props) => {
         </NavLink>
       </li>
       <li>
-        <NavLink to="/u1/places">MY PLACES</NavLink>
+        <NavLink to="/u1/places"> MY PLACES </NavLink>
       </li>
       <li>
-        <NavLink to="/places/new">ADD PLACE</NavLink>
+        <NavLink to="/places/new"> ADD PLACE </NavLink>
       </li>
       <li>
-        <NavLink to="/auth">AUTHENTICATE</NavLink>
+        <NavLink to="/auth"> AUTHENTICATE </NavLink>
       </li>
     </ul>
   );
