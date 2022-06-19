51e45e1a658188f7326d5d6251db2d2f0f1023ae
Emmanuel Oloke <dapopeemmanuel@gmail.com>
2022-06-19 22:35:01 +0100

    impplemented login requests

--- src/user/pages/Auth.js
+++ src/user/pages/Auth.js
@@ -57,10 +57,33 @@ const Auth = () => {
   const authSubmitHandler = async (event) => {
     event.preventDefault();
 
+    setIsLoading(true); // Here we setIsLoading to true because now we are loading and we want the UI to re-render
+
     if (isLoginMode) {
+      try {
+        const response = await fetch('http://localhost:8000/api/users/login', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({
+            email: formState.inputs.email.value,
+            password: formState.inputs.password.value,
+          }),
+        }); // The fecth() API, which is provided by browsers in modern JS and used to send HTTP Requests. It takes a string that points at out Backend code
+
+        const responseData = await response.json(); // This returns a promise, hence why we need await keyword.
+        if (!response.ok) {
+          throw new Error(responseData.message); // If we get a 400/500 status code from the result of the fetch API execution, we make sure to throw an error. Because fetch API by defult technically just returns an error status code and not actually throw an error.
+        }
+        setIsLoading(false); // We first clear the local state by setting isLoading to false before calling auth.login() because the state might change. Otherwise we might be updating a state on a component which is not on the screen anymore.
+        auth.login(); // We only want to call auth.login() if we didn't have an error, hence why we do it here in the try block
+      } catch (err) {
+        setIsLoading(false);
+        setError(err.message || 'Something went wrong, please try again'); // If we have an error while loading we call the setError state.
+      }
     } else {
       try {
-        setIsLoading(true); // Here we setIsLoading to true because now we are loading and we want the UI to re-render
         const response = await fetch('http://localhost:8000/api/users/signup', {
           method: 'POST',
           headers: {
@@ -77,11 +100,9 @@ const Auth = () => {
         if (!response.ok) {
           throw new Error(responseData.message); // If we get a 400/500 status code from the result of the fetch API execution, we make sure to throw an error. Because fetch API by defult technically just returns an error status code and not actually throw an error.
         }
-        console.log(responseData);
         setIsLoading(false); // We first clear the local state by setting isLoading to false before calling auth.login() because the state might change. Otherwise we might be updating a state on a component which is not on the screen anymore.
         auth.login(); // We only want to call auth.login() if we didn't have an error, hence why we do it here in the try block
       } catch (err) {
-        console.log(err);
         setIsLoading(false);
         setError(err.message || 'Something went wrong, please try again'); // If we have an error while loading we call the setError state.
       }
