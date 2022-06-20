687418052b69b76c5c52aa87c57afca76087aacb
Emmanuel Oloke <dapopeemmanuel@gmail.com>
2022-06-20 21:15:18 +0100

    implemented updating place

--- src/places/pages/UpdatePlace.js
+++ src/places/pages/UpdatePlace.js
@@ -1,5 +1,5 @@
-import React, { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
+import React, { useEffect, useState, useContext } from 'react';
+import { useParams, useHistory } from 'react-router-dom'; // useParams to handle route parameters and useHistory to handle user redirection in the app
 
 import Input from '../../shared/components/FormElements/Input';
 import Button from '../../shared/components/FormElements/Button';
@@ -12,12 +12,15 @@ import {
 } from '../../shared/util/validators';
 import { useForm } from '../../shared/hooks/form-hook';
 import { useHttpClient } from '../../shared/hooks/http-hook';
+import { AuthContext } from '../../shared/context/auth-context';
 import './PlaceForm.css';
 
-const UpdatePlace = (props) => {
+const UpdatePlace = () => {
+  const auth = useContext(AuthContext);
   const { isLoading, error, sendRequest, clearError } = useHttpClient(); // Using the custom Http hook to handle state and error management
   const [loadedPlace, setLoadedPlace] = useState();
   const placeId = useParams().placeId;
+  const history = useHistory();
 
   const [formState, inputHandler, setFormData] = useForm(
     {
@@ -39,7 +42,7 @@ const UpdatePlace = (props) => {
       try {
         const responseData = await sendRequest(
           `http://localhost:8000/api/places/${placeId}`
-        );
+        ); // This is a default GET request, so we can omit specifying the GET method
         setLoadedPlace(responseData.place);
         setFormData(
           {
@@ -59,9 +62,22 @@ const UpdatePlace = (props) => {
     fetchPlace();
   }, [sendRequest, placeId, setFormData]);
 
-  const placeUpdateSubmitHandler = (event) => {
+  const placeUpdateSubmitHandler = async (event) => {
     event.preventDefault();
-    console.log(formState.inputs);
+    try {
+      await sendRequest(
+        `http://localhost:8000/api/places/${placeId}`,
+        'PATCH',
+        JSON.stringify({
+          title: formState.inputs.title.value,
+          description: formState.inputs.description.value,
+        }),
+        {
+          'Content-Type': 'application/json',
+        }
+      ); // This is not a default request, so we must attach the PATCH method to it.
+      history.push('/' + auth.userId + '/places'); // Redirected the user to /:userId/places
+    } catch (err) {}
   };
 
   if (isLoading) {
