0b23eb69a2c37d117e9297066a132c537ba14fa1
Emmanuel Oloke <dapopeemmanuel@gmail.com>
2022-06-20 19:59:18 +0100

    loading places by userId

--- src/places/pages/UserPlaces.js
+++ src/places/pages/UserPlaces.js
@@ -1,38 +1,40 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react'; // Using useEffect to make sure we send a request when this component renders but not when it rerenders
 import { useParams } from 'react-router-dom'; // A React hook that gives us access to the dynamic segments of a URL i.e the Route Parameters
 
 import PlaceList from '../components/PlaceList';
+import ErrorModal from '../../shared/components/UIElements/ErrorModal';
+import LoadingSpinner from '../../shared/components/UIElements/LoadingSpinner';
+import { useHttpClient } from '../../shared/hooks/http-hook';
 
-const DUMMY_PLACES = [{
-        id: 'p1',
-        title: 'Empire State Building',
-        description: 'One of the most famous skyscrappers in the world',
-        imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/0/05/View_of_Empire_State_Building_from_Rockefeller_Center_New_York_City_dllu.jpg/640px-View_of_Empire_State_Building_from_Rockefeller_Center_New_York_City_dllu.jpg',
-        address: '20 W 34th St, New York, NY 10001',
-        location: {
-            lat: 40.7484445,
-            lng: -73.9878531,
-        },
-        creator: 'u1',
-    },
-    {
-        id: 'p2',
-        title: 'Emp. State Building',
-        description: 'One of the most famous skyscrappers in the world',
-        imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/0/05/View_of_Empire_State_Building_from_Rockefeller_Center_New_York_City_dllu.jpg/640px-View_of_Empire_State_Building_from_Rockefeller_Center_New_York_City_dllu.jpg',
-        address: '20 W 34th St, New York, NY 10001',
-        location: {
-            lat: 40.7484445,
-            lng: -73.9878531,
-        },
-        creator: 'u2',
-    },
-];
 const UserPlaces = () => {
-    const userId = useParams().userId; // This gives us access to the userId that is encoded in the Route Parameter
-    const loadedPlaces = DUMMY_PLACES.filter((place) => place.creator === userId); // Only returns an array of elements where creator matched the userId Route Parameter
-    return <PlaceList items = { loadedPlaces }
-    />;
+  const [loadedPlaces, setLoadedPlaces] = useState();
+  const { isLoading, error, sendRequest, clearError } = useHttpClient();
+
+  const userId = useParams().userId; // This gives us access to the userId that is encoded in the Route Parameter
+
+  useEffect(() => {
+    const fetchPlaces = async () => {
+      try {
+        const responseData = await sendRequest(
+          `http://localhost:8000/api/places/user/${userId}`
+        );
+        setLoadedPlaces(responseData.places);
+      } catch (err) {}
+    };
+    fetchPlaces(); // We use an IIFE here because it is not recommended to use async on useEffect hook
+  }, [sendRequest, userId]);
+
+  return (
+    <React.Fragment>
+      <ErrorModal error={error} onClear={clearError} />
+      {isLoading && (
+        <div className="center">
+          <LoadingSpinner />
+        </div>
+      )}
+      {!isLoading && loadedPlaces && <PlaceList items={loadedPlaces} />}
+    </React.Fragment>
+  );
 };
 
 export default UserPlaces;
