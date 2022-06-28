1190f562508b24757c93648280d4c6ea6d4fc755
Emmanuel Oloke <dapopeemmanuel@gmail.com>
2022-06-28 03:18:46 +0100

    managing token expiration date

--- src/App.js
+++ src/App.js
@@ -17,13 +17,19 @@ const App = () => {
   const [token, setToken] = useState(false);
   const [userId, setUserId] = useState(false);
 
-  const login = useCallback((uid, token) => {
+  const login = useCallback((uid, token, expirationDate) => {
     setToken(token);
+    setUserId(uid);
+    const tokenExpirationDate =
+      expirationDate || new Date(new Date().getTime() + 1000 * 60 * 60); // This gives us a new date which is current date/time at login time plus 1 hour, since the expiration time we set on the backend is 1 hour
     localStorage.setItem(
       'userData',
-      JSON.stringify({ userId: uid, token: token })
-    ); // Using localStorage which is a browser functionality to store out token, so we don't get logged on every refresh. You can only write text or data that can be converted into text to the localStorage object, that's why we had to use JSON.stringify()
-    setUserId(uid);
+      JSON.stringify({
+        userId: uid,
+        token: token,
+        expiration: tokenExpirationDate.toISOString(),
+      })
+    ); // Using localStorage which is a browser functionality to store our token, so we don't get logged on every refresh. You can only write text or data that can be converted into text to the localStorage object, that's why we had to use JSON.stringify(). We also add the expiration key to store the expiration date which is one hour from login time, and used the .toISOString method to make sure no data gets lost when it's being stringified
   }, []);
 
   const logout = useCallback(() => {
@@ -35,8 +41,16 @@ const App = () => {
   useEffect(() => {
     // Adding a function to check the browser localStorage for a token once the app starts. The dependencies of the function is an empty array which means the function will only run once
     const storedData = JSON.parse(localStorage.getItem('userData')); // We use the getItem method on localStorageand pass in the key which we used to store the token, which in this case is userData. We use the JSON.parse method so we can convert JSON strings to regular JS DS like Object
-    if (storedData && storedData.token) {
-      login(storedData.userId, storedData.token);
+    if (
+      storedData &&
+      storedData.token &&
+      new Date(storedData.expiration) > new Date()
+    ) {
+      login(
+        storedData.userId,
+        storedData.token,
+        new Date(storedData.expiration)
+      );
     }
   }, [login]);
 
